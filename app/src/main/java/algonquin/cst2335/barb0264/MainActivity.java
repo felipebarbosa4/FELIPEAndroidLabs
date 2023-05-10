3f8c2b1da0c750d5a4434177d10a22292c1fca9b
Felipe Figueira <felipe8figueira@gmail.com>
2023-05-10 14:33:19 -0400

    Initial commit

---
+++ app/src/main/java/algonquin/cst2335/barb0264/MainActivity.java
@@ -0,0 +1,13 @@
+package algonquin.cst2335.barb0264;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
