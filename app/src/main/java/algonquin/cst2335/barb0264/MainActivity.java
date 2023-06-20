f9c602bc7a95375887f7484d0478a116020f8ef7
Felipe Figueira <felipe8figueira@gmail.com>
2023-06-20 14:18:33 -0400

    19 Commit : Week 5 - LAB05-START

--- app/src/main/java/algonquin/cst2335/barb0264/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/barb0264/MainActivity.java
@@ -11,30 +11,10 @@ import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgView;
-    Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgView = findViewById(R.id.imageView);
-        sw = findViewById(R.id.spin_switch);
-
-        sw.setOnCheckedChangeListener((btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
-
-        });
     }
 }
